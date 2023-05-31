2aa52000d47eef42b336cbe5d825e679cec3f544
bhavanipassionate <bhavanipassionateart@gmail.com>
2023-05-31 11:02:23 +0200

    first commit

---
+++ helpers/User_helper.js
@@ -0,0 +1,10 @@
+import { randFullName, randEmail } from "@ngneat/falso";
+export const createRandomUser = () => {
+    const data = {
+        email: randEmail({provider: 'jenseneducation', suffix: 'ganga'}),
+        name: randFullName({gender: 'female'}),
+        gender: 'male',
+        status: 'active'
+    };
+    return data;
+}
